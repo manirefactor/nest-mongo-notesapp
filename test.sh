34c9c9a40014b6ac3a6bac8176279e192e33fba2
Manikanta Rayala <106676834+manitrak@users.noreply.github.com>
2022-06-29 09:33:00 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 chmod 400 publickey.pem
-ssh -i "publickey.pem" ubuntu@ec2-3-110-197-38.ap-south-1.compute.amazonaws.com;
+ssh -t -t -i "publickey.pem" ubuntu@ec2-3-110-197-38.ap-south-1.compute.amazonaws.com;
 mkdir mani_test
